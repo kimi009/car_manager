469ace90da9ad4bf4b44a8215e525d6e1235ace3
tangqiulun <tangqiulun@vanvy.cn>
2019-08-20 14:34:52 +0800

    eslint

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,10 +3,23 @@
     <!-- <div class="map-container">
        <a-map></a-map>
     </div> -->
-    home
-    <transition :name="$transition" mode="in-out">
+    <!-- <transition :name="$transition" mode="in-out">
         <router-view/>
-    </transition>
+    </transition> -->
+    <div class="car-info">
+      <div class="left">
+        <p class="car-num">
+          <img src="../../assets/image/home/card.png"
+               alt="">
+          <span></span>
+        </p>
+        <div class="rent"></div>
+        <p class="break">
+          <span>本月违章</span>
+          <span>无</span>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,7 +28,7 @@ import { Button } from 'vant'
 import AMap from '../../components/AMap/amap'
 export default {
   name: 'home',
-  data () {
+  data() {
     return {
       msg: 'Welcome to Your Vue.js Frame',
       value: 3,
@@ -28,22 +41,25 @@ export default {
     [Button.name]: Button,
     AMap
   },
-
-  created () {},
-
+  created() {
+  },
   methods: {
-    getLocation () {}
+    getLocation() { }
   }
 }
 </script>
 
 <style scoped lang="less">
-.home{
-  height: calc(~"100% - 50px");
-  background-color: #FFF;
-  .map-container{
-    .wh(100%, 100%);
+.home {
+  height: calc(~'100% - 50px');
+  background-color: #fff;
+  padding: 0 15px;
+  .car-info {
+    background: linear-gradient(
+      -90deg,
+      rgba(98, 165, 241, 1),
+      rgba(68, 120, 233, 1)
+    );
   }
 }
-
 </style>
