f5320f782bca74a32a35e3e3df06e4f981ecc351
chenfuye <444812002@qq.com>
2019-08-23 15:53:16 +0800

    提交支付功能

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -212,20 +212,21 @@ export default {
         wechatCode: 'jchl_hyc'
       })
       if (configRes.head.errorCode === '0') {
-        let openId = await this.$api.getOpenId({
-          code: 'fhifkfksdjfsdfsfsd',
-          wechatCode: 'jchl_hyc'
-        })
-        console.log(openId)
-        // eslint-disable-next-line
-        wx.config({
-          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-          appId: configRes.appId, // 必填，公众号的唯一标识
-          timestamp: configRes.timestamp, // 必填，生成签名的时间戳
-          nonceStr: configRes.nonceStr, // 必填，生成签名的随机串
-          signature: configRes.signature, // 必填，签名
-          jsApiList: [] // 必填，需要使用的JS接口列表
-        })
+        console.log(configRes)
+        // let openId = await this.$api.getOpenId({
+        //   code: 'aaa',
+        //   wechatCode: 'jchl_hyc'
+        // })
+        // console.log(openId)
+        // // eslint-disable-next-line
+        // wx.config({
+        //   debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        //   appId: configRes.appId, // 必填，公众号的唯一标识
+        //   timestamp: configRes.timestamp, // 必填，生成签名的时间戳
+        //   nonceStr: configRes.nonceStr, // 必填，生成签名的随机串
+        //   signature: configRes.signature, // 必填，签名
+        //   jsApiList: [] // 必填，需要使用的JS接口列表
+        // })
       }
     },
     getLocation() { },
