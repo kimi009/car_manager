9c713b3675f51791489dd100aab3e6e998c97969
1062479924 <1062479924@qq.com>
2019-09-09 16:23:06 +0800

    测试分支

--- config/test.env.js
+++ config/test.env.js
@@ -2,8 +2,8 @@
 module.exports = {
   NODE_ENV: '"testing"',
   EVN_CONFIG: '"test"',
-  HOST: '"http://hyc.jchl.com"',
-  API_ROOT: '"http://hyc.jchl.com/backend/"',
+  HOST: '"http://thyc.jchl.com"',
+  API_ROOT: '"http://thyc.jchl.com/backend/"',
   PSB_ROOT:'"http://test.mypsb.cn/api"',
   DAPI_ROOT: '"http://dapi.jchl.com/"'
 }
